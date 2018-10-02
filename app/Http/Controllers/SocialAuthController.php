ca133df34963f06ecd62e45fadb17923b4376955
Arif Hossain <arif04cuet@gmail.com>
2018-10-02 17:40:23 +0600

    updated

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -37,7 +37,7 @@ class SocialAuthController extends Controller
             'first_name', 'last_name', 'email', 'gender', 'birthday', 'location'
         ])->user();
 
-        dd($socialUser->user);
+        dd($socialUser);
 
       /*
             Gets the user in our database where the provider ID
